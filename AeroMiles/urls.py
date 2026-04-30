be0c23c3361031451dadf82284e54066f7177033
Rafalah7 <rafalahizak@gmail.com>
2026-04-30 11:29:39 +0700

    create simple fe and views

--- AeroMiles/urls.py
+++ AeroMiles/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from members.views import list_member, form_member, dashboard, list_identitas
+from members.views import list_member, form_member, dashboard, list_identitas, redeem_list, buy_package, tier_info, transaction_report
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/list/', list_member, name='list_member'),
     path('members/add/', form_member, name='form_member'),
+    path('members/redeem/', redeem_list, name='redeem_list'),
+    path('members/buy-package/', buy_package, name='buy_package'),
+    path('members/tier/', tier_info, name='tier_info'),
+    path('members/transactions/', transaction_report, name='transaction_report'),
     path('', dashboard, name='dashboard'),
     path('members/identitas/', list_identitas, name='list_identitas'),
 ]
